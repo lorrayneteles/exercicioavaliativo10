cd29b10a6b5e348232c620999b573d7218dc7969
lorrayneteles <lorrayneteles1305@gmail.com>
2024-10-16 18:36:00 -0300

    Add files via upload

---
+++ exercicioavaliativo10.py
@@ -0,0 +1,9 @@
+#Área de um retângulo
+
+largura = int (input("Digite a largura do retângulo:"))
+
+altura = int (input("Digite a altura do retângulo:"))
+
+area = largura * altura
+
+print(f"A área do retângulo é:", area)
